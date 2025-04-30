4726103097b027d7eef3ad1d4acc13aa1f009602
JoseManuelZarcoGarcia <jose.manuel.zarco.garcia@gmail.com>
2025-04-30 12:08:29 +0200

    first commit

---
+++ src/servicios/Cadena.java
@@ -0,0 +1,8 @@
+package servicios;
+
+public class Cadena {
+
+	public boolean contienePalabra(String cadena, String palabra) {
+		 return cadena.contains(palabra); 
+		} 
+}
---
+++ test/servicios/CadenaTest.java
@@ -0,0 +1,25 @@
+package servicios;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class CadenaTest {
+
+	@Test
+	public void verdadero() {
+		Cadena cadena = new Cadena();
+		
+		assertTrue(cadena.contienePalabra("La patata esta negra", "patata"));
+		
+	}
+	
+	@Test
+	public void falso() {
+		Cadena cadena = new Cadena();
+		
+		assertFalse(cadena.contienePalabra("patata", "xmixta"));
+	}
+}
